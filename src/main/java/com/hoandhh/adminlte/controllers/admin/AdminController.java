0d0696bc8eb4ee0ef037e27cc5c14170ae3d5e83
Hoan Doan <hoandoan288@gmail.com>
2024-07-15 17:29:00 +0700

    Update fe category

--- src/main/java/com/hoandhh/adminlte/controllers/admin/AdminController.java
+++ src/main/java/com/hoandhh/adminlte/controllers/admin/AdminController.java
@@ -1,4 +1,4 @@
-package com.hoandhh.adminlte.controller.admin;
+package com.hoandhh.adminlte.controllers.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/hoandhh/adminlte/controllers/admin/CategoryController.java
+++ src/main/java/com/hoandhh/adminlte/controllers/admin/CategoryController.java
@@ -1,2 +1,20 @@
-package com.hoandhh.adminlte.controllers.admin;public class CategoryController {
+package com.hoandhh.adminlte.controllers.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class CategoryController {
+
+    @RequestMapping("/admin/category")
+    public String category() {
+        return "admin/category/index";
+    }
+
+    @RequestMapping("/admin/add-category")
+    public String addCategory() {
+        return "admin/category/add";
+    }
 }
